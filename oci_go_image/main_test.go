47b0a52a0430620653365354e31e62dbe5bd031f
Alex Eagle <alex@aspect.dev>
2025-04-23 12:11:51 -0700

    Update main_test.go

--- oci_go_image/main_test.go
+++ oci_go_image/main_test.go
@@ -8,7 +8,7 @@ import (
 func TestCompare(t *testing.T) {
 	result := Compare("this", "that")
 
-	if !strings.Contains(result, "this") {
+	if !strings.Contains(result, "other") {
 		t.Error("expected a diff containing 'that' but got", result)
 	}
 }
