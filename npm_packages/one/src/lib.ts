8fa7825a2c47c3e06066ab544729bd3e62216b5e
Matt Cramer <matt@aspect.build>
2026-02-10 11:23:57 -0700

    fix fmt

--- npm_packages/one/src/lib.ts
+++ npm_packages/one/src/lib.ts
@@ -1,5 +1,4 @@
 import { format } from 'node:util';
 export function one() {
-   console.log(format('I am %s, not %s!', 'One', 'Two'));
+  console.log(format('I am %s, not %s!', 'One', 'Two'));
 }
-
